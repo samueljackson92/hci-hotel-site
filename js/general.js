47aedc83fe6bb19df649f4b79a1438fb9279a6ae
Samuel Jackson <samueljackson@outlook.com>
2013-03-31 15:44:18 +0100

    All files now run through validator.

--- js/general.js
+++ js/general.js
@@ -17,13 +17,19 @@ function buildDatePicker() {
 	var months = [ "January", "February", "March", "April", "May", "June", 
                "July", "August", "September", "October", "November", "December" ];
 
+    $('.date-num').html("");
+
 	for (i=1; i <= 31; i++) {
 		$('.date-num').append('<option value="'+i+'">'+i+'</option>');
 	}
 
+    $('.date-month').html("");
+
 	for (i=0; i < months.length; i++) {
 		$('.date-month').append('<option value="'+months[i]+'">'+months[i]+'</option>');
 	}
+
+    $('.date-year').html("");
 
 	for (i=2013; i < 2018; i++) {
 		$('.date-year').append('<option value="'+i+'">'+i+'</option>');
